fc043aaa3f81ecb2a2135f39573eba77d44a471f
Toufeeq khan <164334991+Toufeeqkhan@users.noreply.github.com>
2024-10-13 04:25:42 -0700

    Updates

--- assets/index-0scvBtQp.js
+++ assets/index-0scvBtQp.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))d(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const c of a.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&d(c)}).observe(document,{childList:!0,subtree:!0});function n(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function d(e){if(e.ep)return;e.ep=!0;const a=n(e);fetch(e.href,a)}})();const r=async s=>{const n=`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${s}?key=7MM7GJLC8459FAZCUADPLRZC7`,d=await fetch(n);if(d.ok){const e=await d.json();return o(e)}},o=s=>({fullData:s,address:s.address,currentDay:s.days[0],currentDayTemperature:s.days[0].temp,currentDayFeelsLike:s.days[0].feelslike,currentDayDateTime:s.days[0].datetime,currentDayConditions:s.days[0].conditions,currentDayDescription:s.days[0].description,currentDayWindSpeed:s.days[0].windspeed,currentDayHumidity:s.days[0].humidity,currentDaySunrise:s.days[0].sunrise,currentDaySunset:s.days[0].sunset,currentDayHours:s.days[0].hours,chancesOfRain:s.days[0].precipprob,UVIndex:s.days[0].uvindex,weekDays:s.days});class l{constructor(){this.element=document.createElement("nav"),this.render()}render(){this.element.innerHTML=`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))d(s);new MutationObserver(s=>{for(const i of s)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&d(c)}).observe(document,{childList:!0,subtree:!0});function n(s){const i={};return s.integrity&&(i.integrity=s.integrity),s.referrerPolicy&&(i.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?i.credentials="include":s.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function d(s){if(s.ep)return;s.ep=!0;const i=n(s);fetch(s.href,i)}})();const r=async e=>{const n=`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${e}?key=7MM7GJLC8459FAZCUADPLRZC7`,d=await fetch(n);if(d.ok){const s=await d.json();return o(s)}},o=e=>({fullData:e,address:e.address,currentDay:e.days[0],currentDayTemperature:e.days[0].temp,currentDayFeelsLike:e.days[0].feelslike,currentDayDateTime:e.days[0].datetime,currentDayConditions:e.days[0].conditions,currentDayDescription:e.days[0].description,currentDayWindSpeed:e.days[0].windspeed,currentDayHumidity:e.days[0].humidity,currentDaySunrise:e.days[0].sunrise,currentDaySunset:e.days[0].sunset,currentDayHours:e.days[0].hours,chancesOfRain:e.days[0].precipprob,UVIndex:e.days[0].uvindex,weekDays:e.days});class l{constructor(){this.element=document.createElement("nav"),this.render()}render(){this.element.innerHTML=`
           <form action="" id="searchCityForm">
             <input
               autocomplete="off"
@@ -22,7 +22,7 @@
               </div>
             </div>
             <div class="img-section">
-              <img src="./assets/images/sun.svg" alt="Sun" />
+              <img src="./images/sun.svg" alt="Sun" />
             </div>
           </div>
 
@@ -31,32 +31,32 @@
             <div class="hours">
               <div class="hour">
                 <p>06:00 AM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[6].temp}</p>
               </div>
               <div class="hour">
                 <p>09:00 AM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[9].temp}</p>
               </div>
               <div class="hour">
                 <p>12:00 PM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[12].temp}</p>
               </div>
               <div class="hour">
                 <p>3:00 PM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[15].temp}</p>
               </div>
               <div class="hour">
                 <p>06:00 PM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[18].temp}</p>
               </div>
               <div class="hour">
                 <p>09:00 PM</p>
-                <img src="./assets/images/sun.svg" alt="sun" />
+                <img src="./images/sun.svg" alt="sun" />
                 <p class="hourly-temp">${this.data.currentDayHours[21].temp}</p>
               </div>
             </div>
@@ -66,28 +66,28 @@
             <p>AIR CONDITIONS</p>
             <div class="conditions">
               <div class="condition">
-                <img src="./assets/images/feel.svg" alt="Feel" />
+                <img src="./images/feel.svg" alt="Feel" />
                 <div class="info">
                   <h4>Feel Like</h4>
                   <p>${this.data.currentDayFeelsLike}</p>
                 </div>
               </div>
               <div class="condition">
-                <img src="./assets/images/wind.svg" alt="Wind" />
+                <img src="./images/wind.svg" alt="Wind" />
                 <div class="info">
                   <h4>Wind</h4>
                   <p>${this.data.currentDayWindSpeed}mph</p>
                 </div>
               </div>
               <div class="condition">
-                <img src="./assets/images/rain.svg" alt="Rain" />
+                <img src="./images/rain.svg" alt="Rain" />
                 <div class="info">
                   <h4>Chances of Rain</h4>
                   <p>${this.data.chancesOfRain}%</p>
                 </div>
               </div>
               <div class="condition">
-                <img src="./assets/images/UV.svg" alt="UV" />
+                <img src="./images/UV.svg" alt="UV" />
                 <div class="info">
                   <h4>UV Index</h4>
                   <p>${this.data.UVIndex}%</p>
@@ -135,4 +135,4 @@
             <p class="temp"><strong>${this.data.weekDays[7].temp}</strong>/${this.data.weekDays[7].tempmin}</p>
           </div>
         </div>
-        `}}const h=new l,i=document.createElement("main"),u=async s=>{const t=await r(s);i.innerHTML="";const n=new p(t),d=new m(t);i.appendChild(n.element),i.appendChild(d.element)};document.addEventListener("DOMContentLoaded",()=>{const s=document.getElementById("app");s.appendChild(h.element),i.innerHTML="<h1>Please Enter the City name</h1>",i.classList.add("temp-h1"),s.appendChild(i),document.getElementById("searchCityForm").addEventListener("submit",t=>{t.preventDefault();const n=document.getElementById("location").value;i.innerHTML="<h1>Please wait</h1>",i.classList.remove("temp-h1"),u(n)})});
+        `}}const h=new l,a=document.createElement("main"),u=async e=>{const t=await r(e);a.innerHTML="";const n=new p(t),d=new m(t);a.appendChild(n.element),a.appendChild(d.element)};document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("app");e.appendChild(h.element),a.innerHTML="<h1>Please Enter the City name</h1>",a.classList.add("temp-h1"),e.appendChild(a),document.getElementById("searchCityForm").addEventListener("submit",t=>{t.preventDefault();const n=document.getElementById("location").value;a.innerHTML="<h1>Please wait</h1>",a.classList.remove("temp-h1"),u(n)})});
